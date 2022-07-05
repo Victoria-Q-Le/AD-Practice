97686d2b23b4a3ab5c04c25a34529cbc61acd9c3
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-07-05 12:39:17 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,1 @@
+// Write a function called same, which accepts 2 arrays. The function should return true if every value in the array has its corresponding value squared in the second array. The frequency of values must be the same. 
