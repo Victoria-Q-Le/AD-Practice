86f5783f90f6b16526ebff22d5cf8278b71981c6
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-07-05 20:03:29 -0400

    solution 1

--- multilePointers.js
+++ multilePointers.js
@@ -1,1 +1,14 @@
-// Write a function call sumZero which accepts a sorted array of integers. The function should find ther first pair where the sum is 0. Return an array that includes both values that sum to zero or undefinded if a pair does not existed 
+// Write a function call sumZero which accepts a sorted array of integers. The function should find ther first pair where the sum is 0. Return an array that includes both values that sum to zero or undefinded if a pair does not existed
+
+
+// Solution 1:
+function sumZero (arr) {
+  for (let i = 0; i < arr.length; i++){
+    for (let j = i + 1; j < arr.length; j++){
+      if (arr[i] + arr[j] === 0){
+        return [arr[i], arr[j]]
+      }
+    }
+  }
+}
+// this solution is not ideal because it is O(n2) time complexity 
