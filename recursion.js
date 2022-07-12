3c2b7ed344cb8ba073426dca8da1f69ebf21f666
Victoria Le <leqvictoria@gmail.com>
2022-07-12 16:11:30 -0400

    recursive helper function

--- recursion.js
+++ recursion.js
@@ -21,7 +21,26 @@
 //  //     => return 6 in total
 
 
-function factorial(num) {
-  if (num === 1) return 1;
-  return num * factorial(num - 1);
+// function factorial(num) {
+//   if (num === 1) return 1;
+//   return num * factorial(num - 1);
+// }
+
+
+// Write a function to collect the odd values inside an array
+function collectOddValues (arr){
+  let result = [];
+  // Define recursive helper function within the outer function
+  function helper (helperInput){
+    if (helperInput.length === 0){
+      return
+    }
+    if (helperInput[0] % 2 !== 0){
+      result.push(helperInput[0])
+    }
+    helper(helperInput.slice(1))
+  }
+  helper(arr)
+  // The end of recursive helper function
+  return result
 }
