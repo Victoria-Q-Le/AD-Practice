0c5d7e24950ec0e61ddd5f547f7a08c9451ee8bc
Victoria Le <leqvictoria@gmail.com>
2022-07-15 18:34:19 -0400

    binary search

--- searchingAlgorithms.js
+++ searchingAlgorithms.js
@@ -6,3 +6,22 @@ function linearSearch (arr,val){
   }
   return -1
 }
+
+// Binary Search Pseudocode
+// This function accepts a sorted array and a value. Create a left pointerat the start of the array, and a right pointer at the end of the array. While the left pointer comes before the right pointer:
+// Create a pointer in the middle
+// If you find the value you want, return the index
+// If the value is too small, move the left point up
+// If the value is too large, move the right pointer down
+// If you never find the value, return -1
+function binarySearch (arr, val){
+  let start = 0
+  let end = arr.length - 1
+  let middle = Math.floor((end + start)/2)
+  while (arr[middle] !== val && start <= end){
+    if (val < arr[middle]) end = middle - 1
+    else start = middle + 1
+    middle = Math.floor((start + end)/2)
+  }
+  return arr[middle] === val ? middle : -1
+}
