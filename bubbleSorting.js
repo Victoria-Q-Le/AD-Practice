c252bb1b1bc0e4632767e640faf9a311f56f84f9
Victoria Le <leqvictoria@gmail.com>
2022-08-10 11:57:44 -0400

    optimize bubblesort

--- bubbleSorting.js
+++ bubbleSorting.js
@@ -12,4 +12,20 @@ function bubbleSort (arr) {
   }
   return arr
 }
-// this solution is not optimize because the j runs through the entire array. 
+// this solution is not optimize because the j runs through the entire array.
+
+// OPTIMIZE:
+function bubbleSort (arr) {
+  for (let i = arr.length; i > 0; i--){
+    for (let j = 0; j < i - 1; j++){
+      console.log(arr, arr[j],arr[j+1]);
+      if (arr[j] > arr[j+1]){
+        // SWAP
+        let temp = arr[j]
+        arr[j] = arr[j+1]
+        arr[j+1] = temp
+      }
+    }
+  }
+  return arr
+}
