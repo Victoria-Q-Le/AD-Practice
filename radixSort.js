7d032032ce9bb0087cae5edaf857a0766541449d
Victoria Le <leqvictoria@gmail.com>
2022-08-18 15:19:07 -0400

    radixSort constructed

--- radixSort.js
+++ radixSort.js
@@ -15,3 +15,19 @@ function mostDigits(nums){
    }
    return maxDigits
 }
+
+function radixSort(nums){ //define a function that accepts list of numbers
+  let mostDigitCount = mostDigits(nums) //figure out how many digits the largest number has
+  //console.log(mostDigitCount);
+  for (let k = 0; k < mostDigitCount; k++){ //loop from k = 0 up to this largest number of digits
+    let digitBuckets = Array.from({length: 10}, () => []) //create an array with 10 elements, and each element will be an empty sub arr.
+    for (let i = 0; i < nums.length; i++) { //for each i of the loop:
+      let digit = getDigit(nums[i],k) //get the k-th digit of nums[i]
+      digitBuckets[digit].push(nums[i]) //push the number with same k-digit to the same digitBuckets sub arr (i.e: 23 and 123 belong to the same digitBuckets when k = 0, 123 and 225 belong to the same bucket when k = 1 )
+    }
+    console.log(digitBuckets);
+    nums = [].concat(...digitBuckets) //concat the sub arrays in digitBuckets array together, using the ... operator allows the values to be joined in a same array.
+    console.log(nums);
+  }
+  return nums
+}
