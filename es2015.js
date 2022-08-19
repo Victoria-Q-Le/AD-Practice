a8db800ffd04bc0dfd9e32216c4ec3be9278cad6
Victoria Le <leqvictoria@gmail.com>
2022-08-19 13:52:19 -0400

    recap

--- es2015.js
+++ es2015.js
@@ -39,3 +39,30 @@ let firstStudent = new Student("Victoria", "Le", 4)
 firstStudent.fullName()
 firstStudent.markLate() // 1
 firstStudent.addScore(92)
+
+
+////////////////Adding Class Methods/////////////////////
+// -Using the "static" key word in front of the method definition
+// -Define method and functionality that permanent to classes but not neccessary to the individual instances
+class Point {
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+  }
+  static distance (a,b){
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+
+    return Math.hypot(dx,dy)
+  }
+}
+ const p1 = new Point(5,5)
+ const p2 = new Point(10,10)
+
+
+///////////////////////////////RECAP/////////////////////////
+// - Classes are blueprints that when created make objects known as instances
+// - Classes are created with the new keyword
+// - The constructor function is a special function that gets run when the class is instantiated.
+// - Instance methods can be added to classes similar to methods in objects
+// - Class methods can be added using static keyword
