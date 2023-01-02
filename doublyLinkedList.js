180e83e3f31be2a8adeb26cac2bfa2c24fb8fb40
Victoria Le <leqvictoria@gmail.com>
2023-01-02 14:23:17 -0500

    constructing the node and doubly linked list

---
+++ doublyLinkedList.js
@@ -0,0 +1,18 @@
+///////Comparisons with Singly Linked Lists//////
+// - More memory === more flexibility 
+
+class Node {
+    constructor (val){
+        this.val = val 
+        this.next = null 
+        this.prev = null
+    }
+}
+
+class doublyLinkedList{
+    constructor(){
+        this.head = null 
+        this.tail = null 
+        this.length = 0
+    }
+}
