31d85b8b17540e4e07f5e7f37a507d0ad38e4fd9
Victoria Le <leqvictoria@gmail.com>
2023-01-02 15:48:50 -0500

    push method

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -9,10 +9,24 @@ class Node {
     }
 }
 
-class doublyLinkedList{
+class DoublyLinkedList{
     constructor(){
         this.head = null 
         this.tail = null 
         this.length = 0
+    }
+    push(val){ //taking a value and adding it to the end of the list 
+        const newNode = new Node (val)    // create a new node with the value passed to the function
+        if (!this.length === 0) {// if the head property is null set the head and tail to be the newly created node 
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode // if not, set the next property on the tail to be that node 
+            newNode.prev = this.tail // set the prev property on the newly created node to be the the tail 
+            this.tail = newNode  // set the tail to be the newly created node 
+        }
+        this.length++// increment the length 
+        return this // return the list 
+
     }
 }
