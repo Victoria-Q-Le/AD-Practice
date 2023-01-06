b85ae7f40b36b2ae4ff7c5f3eb6842cff6c58746
Victoria Le <leqvictoria@gmail.com>
2023-01-06 15:47:49 -0500

    BST

---
+++ trees.js
@@ -0,0 +1,33 @@
+////////////////////////TREES////////////////////////
+What is a Tree 
+    a data structure that consists of nodes in a parent/child relationship
+
+Compare and contract trees and lists
+    Lists - linear 
+    Trees - nonlinear 
+
+Explain the differences between trees, binary trees and binary search trees 
+    Terminology:
+        ROOT - The top node in the tree 
+        CHILD - A node directly connected to another node when moving away from the Root  
+        Parent - The converse notion of a child
+        Siblings - A group of nodes with the same parent 
+        Leaf - A node with no children
+        Edge - The connection between one node and another 
+
+    Use cases: 
+        HTML DOM
+        Network Routing 
+        Abstract Syntax Tree 
+        Artificial Intelligence (tic tqac toe - minimax )
+        Folders in operating systems, computer file systems
+        JSON
+    
+    Binary Trees: one parent can only have up to 2 children 
+
+    BST 
+        A binary trees whose data can be sortable (usually number), 
+        Every node on the left of the parent is always less than the parent
+        Every node to the right of a parent node is always greater than the parent
+
+Implement operations on binary search trees (BST)
