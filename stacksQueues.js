7456e2b323448521c7b39b2bbe06987ed85eaf59
Victoria Le <leqvictoria@gmail.com>
2023-01-06 14:33:06 -0500

    stacks for singly linked list

--- stacksQueues.js
+++ stacksQueues.js
@@ -1,6 +1,49 @@
-STACKS 
-What is a stack?
-    Data collections, abstract data structure, LIFO data structure (last in first out) - the last element added to the stack will be the first element removed from the stack
-Whare stacks are used?
-    Managin function invocations 
-    Undo/Redo 
+//////////////////////////////STACKS /////////////////////////////
+// What is a stack?
+//     Data collections, abstract data structure, LIFO data structure (last in first out) - the last element added to the stack will be the first element removed from the stack
+// Whare stacks are used?
+//     Managin function invocations 
+//     Undo/Redo 
+//     Routing (the history object) is treated like a stack 
+// Stack implementations
+//     Push and Pop for array 
+//     Shift and unShift for linked lists 
+
+
+class Node {
+    constructor(value){
+        this.value = value 
+        this.next = null 
+    }
+}
+
+class Stack {
+    constructor(){
+        this.first = null 
+        this.last = null
+        this.size = 0
+    }
+    push(value){ //basically this is a unshift in linked list, because we want constant time 
+        const newNode = new Node (value)
+        if(!this.first){
+            this.first = newNode
+            this.last = newNode
+        } else {
+            const temp = this.first
+            this.first = newNode
+            this.first.next = temp 
+
+        }
+        return ++this.size
+    }
+    pop(){
+        if (!this.first) return null 
+        const temp = this.first
+        if (this.first === this.last){
+            this.last = null
+        }
+        this.first = this.first.next
+        this.size--
+        return temp.value
+    }
+}
