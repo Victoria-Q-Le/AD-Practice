407aedad049cca9e91963ae3cc55325be4946c00
Victoria Le <leqvictoria@gmail.com>
2023-01-06 15:11:08 -0500

    queue enqueue and dequeue

--- queues.js
+++ queues.js
@@ -25,4 +25,25 @@ class Queue {
         this.last = null 
         this.size = 0
     }
+    enqueue(value){ //adding to the end of the queue 
+        const newNode = new Node (value)
+        if (this.size === 0){
+            this.first = newNode 
+            this.last = newNode
+        } else {
+            this.last.next = newNode
+            this.last = newNode
+        }
+        return ++this.size
+    }
+    dequeue(){ //remove from the begining of the queue
+        if (this.size === 0) return null 
+        const temp = this.first 
+        if (this.first === this.last){
+            this.last = null
+        }
+        this.first = this.first.next
+        this.size--
+        return temp.value
+    }
 }
