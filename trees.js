2cc98f23c583b948b92e82a5a917d86e9673c3db
Victoria Le <leqvictoria@gmail.com>
2023-02-22 09:10:56 -0500

    big O of tree

--- trees.js
+++ trees.js
@@ -95,3 +95,7 @@ tree.root.right = new Node (15)
 tree.root.left = new Node (7)
 tree.root.left.right = new Node(9)
 tree.root.right
+
+//////BIG O///////
+// Insertion - O(log n)
+// Searching - O(log n)
