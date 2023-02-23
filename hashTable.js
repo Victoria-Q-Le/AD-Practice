efb1882b52f8770261578e37102ac7ef5f4bf13d
Victoria Le <leqvictoria@gmail.com>
2023-02-23 17:19:02 -0500

    hash table class

--- hashTable.js
+++ hashTable.js
@@ -7,9 +7,9 @@
 // Why?
 
 //What makes a good hash?
-1. Fast 
-2. doesnt cluster outputs at specific indices, but distributes uniformly 
-3. deterministic (same input yields same output)
+// 1. Fast 
+// 2. doesnt cluster outputs at specific indices, but distributes uniformly 
+// 3. deterministic (same input yields same output)
 
 //.charCodeAt() = giving the numeric position of specific letter
 
@@ -21,4 +21,39 @@ function hash(key, arrayLength){
         total = (total + value) % arrayLength
     }
     return total 
+}
+// Whats wrong with this hash?
+// - Only hash string 
+// - Not constant time - linear in key length
+// - Could be a little more random
+
+function hashImprovised(key, arrayLength){ //arrayLength is the amount of locker we have 
+    let total = 0
+    let prime = 31 //adding a prime number to the length can significantly reduce the cluster and collision
+    for (let i = 0; i < Math.min(key.length, 100); i++){ //Math.min to ensure the run time is always less than 100
+        let char = key[i]
+        let value = char.charCodeAt(0) - 96
+        total = (total * prime + value) % arrayLength
+    }
+    return total
+}
+
+///////////HANDLING COLLISIONS/////////////
+// 1. Seperate Chaining: at each index in our array we store values using more sophisticated data structure (and array or a linked list - storing diff key value pair in the same locker)
+// 2. Linear Probing: when we find the collision, we search through the array to find the next empty slot. What if no empty locker? How do we know which one we decided to move to a diff locker and which one?
+
+class HashTable {
+    constructor(size =53){
+        this.keyMap = new Array(size)
+    }
+    _hash(key){
+        let total = 0
+        let prime = 31 
+        for (let i =0; i < Math.min(key.length, 100); i++){
+            let char = key[i]
+            let value = char.charCodeAt(0) - 96
+            total = (total * prime + value) % this.keyMap.length
+        }
+        return total
+    }
 }
